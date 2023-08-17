f9d232c00d7f62e3b0667696c241d01bd46cbcc2
Gina Krieb <ginakrieb@Ginas-MacBook-Pro-2.local>
2023-08-17 13:35:16 -0500

    Add some initial error handling on user and product

--- src/server.ts
+++ src/server.ts
@@ -28,4 +28,14 @@ app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signIn);
 
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "Unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "Invalid input" });
+  } else {
+    res.status(500).json({ message: "Internal error" });
+  }
+});
+
 export default app;
